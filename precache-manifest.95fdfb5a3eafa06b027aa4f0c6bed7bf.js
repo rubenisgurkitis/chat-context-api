513d57c534dcbf7437ad1ecbc2aee3866d4a83d1
Ruben Agullo Cordoba <rubenagullocordoba@sysadminsMBP48.fritz.box>
2019-05-08 15:18:32 +0200

    Updates

---
+++ precache-manifest.95fdfb5a3eafa06b027aa4f0c6bed7bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc685713e626e3c81d199e0752a99d64",
+    "url": "/chat-context-api/index.html"
+  },
+  {
+    "revision": "ea4305d37535989b3817",
+    "url": "/chat-context-api/static/css/main.92c281d1.chunk.css"
+  },
+  {
+    "revision": "5694e1898ca4c92fd14d",
+    "url": "/chat-context-api/static/js/2.6408f43b.chunk.js"
+  },
+  {
+    "revision": "ea4305d37535989b3817",
+    "url": "/chat-context-api/static/js/main.0e236866.chunk.js"
+  },
+  {
+    "revision": "12c3b60e7e747bb239dc",
+    "url": "/chat-context-api/static/js/runtime~main.2e4708d6.js"
+  },
+  {
+    "revision": "473718d4c5a0b790158686416c03800b",
+    "url": "/chat-context-api/static/media/chat-background.473718d4.png"
+  }
+]);
